ce9b414e310fe8ba7cde54b9f8e3ce7f96c7c3a7
Mithun Kumar S R <mithunkumar.sr@gmail.com>
2018-10-08 02:41:14 +0530

    Add files via upload

---
+++ PlayGround/TheInitialBot.py
@@ -0,0 +1,45 @@
+# The initial bot
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a function that responds to a user's message: respond
+def respond(message):
+    # Concatenate the user's message to the end of a standard bot respone
+    bot_message = "I can hear you! You said: " + message
+    # Return the result
+    return bot_message
+
+# Define a function that sends a message to the bot: send_message
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
+# Send a message to the bot
+send_message("hello")
+
+# Define variables
+name = "Mithun"
+weather = "Rainy"
+
+# Define a dictionary with the predefined responses
+responses = {
+    "what's your name?": "my name is {0}".format(name),
+    "what's today's weather?": "the weather is {0}".format(weather),
+    "default": "default message"
+}
+
+
+# Return the matching response if there is one, default otherwise
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return the matching message
+        bot_message = responses[message]
+    else:
+        # Return the "default" message
+        bot_message = responses["default"]
+    return bot_message
